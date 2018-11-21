fc483a92b9dcab3105473810dd04cdab37a777ab
relberger <relberger96@gmail.com>
2018-11-20 19:08:16 -0500

    class 11/20

--- PerpetualMotion/app/src/main/java/com/example/rachel/perpetualmotion/activities/MainActivity.java
+++ PerpetualMotion/app/src/main/java/com/example/rachel/perpetualmotion/activities/MainActivity.java
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.rachel.perpetualmotion.R;
 
@@ -57,5 +57,13 @@ public class MainActivity extends AppCompatActivity
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void turn_action_discard(View view)
+	{
+	}
+
+	public void turn_action_deal(View view)
+	{
 	}
 }
